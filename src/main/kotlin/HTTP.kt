bf331e35c631d8912caa1dd63594aae03d5f3601
planby-hyeonjunpark <hyeonjun.park@planby.us>
2025-05-13 15:36:22 +0900

    * 제거

--- src/main/kotlin/HTTP.kt
+++ src/main/kotlin/HTTP.kt
@@ -1,10 +1,12 @@
 package com.example
 
-import io.ktor.http.*
-import io.ktor.server.application.*
-import io.ktor.server.plugins.cors.routing.*
-import io.ktor.server.plugins.swagger.*
-import io.ktor.server.routing.*
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.plugins.cors.routing.CORS
+import io.ktor.server.plugins.swagger.swaggerUI
+import io.ktor.server.routing.routing
 
 fun Application.configureHTTP() {
     install(CORS) {
